532e8d983939d4a432acd4fd7e7fc906ba36d329
rajitha-00 <rajithatw07102@gmail.com>
2026-06-22 14:37:59 +0530

    done

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,7 @@ import "swiper/css/autoplay";
 
 import ReduxProviderWrapper from "@/lib/ReduxProviderWrapper";
 import Preloader from "@/layouts/Preloader";
+import MaintenancePage from "@/components/Down";
 
 export const metadata = {
   title:
@@ -387,9 +388,9 @@ export default function RootLayout({ children }) {
       <body>
         <ReduxProviderWrapper>
           <Preloader />
-          {children}
+          {/* {children} */}
           {/* Creative Maintenance Page */}
-          {/* <MaintenancePage /> */}
+          <MaintenancePage />
         </ReduxProviderWrapper>
       </body>
     </html>
